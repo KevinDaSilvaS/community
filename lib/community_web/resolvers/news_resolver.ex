aa3c4242bf605f2a182740d2c839000321994d36
root <kevin.silva@sensedia.com>
2022-04-12 23:48:20 +0000

    refactor resolver

--- lib/community_web/resolvers/news_resolver.ex
+++ lib/community_web/resolvers/news_resolver.ex
@@ -4,10 +4,8 @@ defmodule CommunityWeb.NewsResolver do
     {:ok, Mock_DB.get_data()}
   end
 
-  def create_link(_root, args, _info) do
+  def create_link(_root, %{:description => desc, :url => url}, _info) do
     %_{description: _d, id: id, url: _url} = Mock_DB.get_data() |> List.last()
-    desc = Map.fetch! args, :description
-    url  = Map.fetch! args, :url
 
     link = %Link{id: id+1, url: url, description: desc}
     Mock_DB.insert_data(link)
