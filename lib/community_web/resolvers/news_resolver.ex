e123c9c3fea37265e5be4ebed1246ac3e4fba313
root <kevin.silva@sensedia.com>
2022-04-12 23:44:26 +0000

    add create_link resolver

--- lib/community_web/resolvers/news_resolver.ex
+++ lib/community_web/resolvers/news_resolver.ex
@@ -1,7 +1,16 @@
 defmodule CommunityWeb.NewsResolver do
-  alias Community.News
 
   def all_links(_root, _args, _info) do
     {:ok, Mock_DB.get_data()}
+  end
+
+  def create_link(_root, args, _info) do
+    %_{description: _d, id: id, url: _url} = Mock_DB.get_data() |> List.last()
+    desc = Map.fetch! args, :description
+    url  = Map.fetch! args, :url
+
+    link = %Link{id: id+1, url: url, description: desc}
+    Mock_DB.insert_data(link)
+    {:ok, link}
   end
 end
